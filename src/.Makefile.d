fcff9b7c42fbc32c696500bda42ce6a2a0bae435
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2026-06-21 19:08:02 -0400

    Remove bad file.

--- src/.Makefile.d
+++
@@ -1,8 +0,0 @@
-basics.vo basics.glob basics.v.beautified basics.required_vo: basics.v /nix/store/4dcnc799l4rpyc8cdmq690v0h18imafa-rocq-9.0.1/lib/rocq-runtime/rocqworker
-basics.vos basics.vok basics.required_vos: basics.v /nix/store/4dcnc799l4rpyc8cdmq690v0h18imafa-rocq-9.0.1/lib/rocq-runtime/rocqworker
-sorting.vo sorting.glob sorting.v.beautified sorting.required_vo: sorting.v /nix/store/4dcnc799l4rpyc8cdmq690v0h18imafa-rocq-9.0.1/lib/rocq-runtime/rocqworker
-sorting.vos sorting.vok sorting.required_vos: sorting.v /nix/store/4dcnc799l4rpyc8cdmq690v0h18imafa-rocq-9.0.1/lib/rocq-runtime/rocqworker
-imp.vo imp.glob imp.v.beautified imp.required_vo: imp.v /nix/store/4dcnc799l4rpyc8cdmq690v0h18imafa-rocq-9.0.1/lib/rocq-runtime/rocqworker
-imp.vos imp.vok imp.required_vos: imp.v /nix/store/4dcnc799l4rpyc8cdmq690v0h18imafa-rocq-9.0.1/lib/rocq-runtime/rocqworker
-opt.vo opt.glob opt.v.beautified opt.required_vo: opt.v imp.vo /nix/store/4dcnc799l4rpyc8cdmq690v0h18imafa-rocq-9.0.1/lib/rocq-runtime/rocqworker
-opt.vos opt.vok opt.required_vos: opt.v imp.vos /nix/store/4dcnc799l4rpyc8cdmq690v0h18imafa-rocq-9.0.1/lib/rocq-runtime/rocqworker
