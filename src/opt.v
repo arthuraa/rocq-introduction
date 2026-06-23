e2a79513dff3e3f56a37cce240e1d590d6039ed6
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2026-06-23 06:50:09 -0700

    Convert optimization lecture to simplified style.

--- src/opt.v
+++ src/opt.v
@@ -80,92 +80,97 @@ Fixpoint cp_com c :=
   | While e c => cp_while (cp_expr e) (cp_com c)
   end.
 
-Inductive cp_add_spec : expr -> expr -> expr -> Prop :=
-| CpAdd1 n1 n2 : cp_add_spec (Num n1) (Num n2) (Num (n1 + n2))
-| CpAdd2 e : cp_add_spec (Num 0) e e
-| CpAdd3 e : cp_add_spec e (Num 0) e
-| CpAdd4 e1 e2 : cp_add_spec e1 e2 (Binop Add e1 e2).
+Definition cp_add_spec (e1 e2 e : expr) : Prop :=
+  (exists n1 n2, e1 = Num n1 /\ e2 = Num n2 /\ e = Num (n1 + n2)) \/
+  (e1 = Num 0 /\ e2 = e) \/
+  (e1 = e /\ e2 = Num 0) \/
+  (e = Binop Add e1 e2).
 
 Lemma cp_addP e1 e2 : cp_add_spec e1 e2 (cp_add e1 e2).
 Proof.
-case: e1 => [x1|[|n1]|b1 e11 e12] /=;
-case: e2 => [x2|[|n2]|b2 e21 e22] /=;
-eauto using cp_add_spec.
+rewrite /cp_add_spec.
+destruct e1 as [x1|[|n1]|b1 e11 e12];
+destruct e2 as [x2|[|n2]|b2 e21 e22];
+eauto 10.
 Qed.
 
-Inductive cp_mul_spec : expr -> expr -> expr -> Prop :=
-| CpMul1 n1 n2 : cp_mul_spec (Num n1) (Num n2) (Num (n1 * n2))
-| CpMul2 e : cp_mul_spec (Num 0) e (Num 0)
-| CpMul3 e : cp_mul_spec e (Num 0) (Num 0)
-| CpMul4 e : cp_mul_spec (Num 1) e e
-| CpMul5 e : cp_mul_spec e (Num 1) e
-| CpMul6 e1 e2 : cp_mul_spec e1 e2 (Binop Mul e1 e2).
+Definition cp_mul_spec (e1 e2 e : expr) : Prop :=
+  (exists n1 n2, e1 = Num n1 /\ e2 = Num n2 /\ e = Num (n1 * n2)) \/
+  (e1 = Num 0 /\ e = Num 0) \/
+  (e2 = Num 0 /\ e = Num 0) \/
+  (e1 = Num 1 /\ e2 = e) \/
+  (e1 = e /\ e2 = Num 1) \/
+  (e = Binop Mul e1 e2).
 
 Lemma cp_mulP e1 e2 : cp_mul_spec e1 e2 (cp_mul e1 e2).
 Proof.
-case: e1 => [x1|[|[|n1]]|b1 e11 e12];
-case: e2 => [x2|[|[|n2]]|b2 e21 e22];
-eauto using cp_mul_spec.
+rewrite /cp_mul_spec.
+destruct e1 as [x1|[|[|n1]]|b1 e11 e12]; eauto;
+destruct e2 as [x2|[|[|n2]]|b2 e21 e22]; eauto 10.
 Qed.
 
-Inductive cp_sub_spec : expr -> expr -> expr -> Prop :=
-| CpSub1 n1 n2 : cp_sub_spec (Num n1) (Num n2) (Num (n1 - n2))
-| CpSub2 e : cp_sub_spec (Num 0) e (Num 0)
-| CpSub3 e : cp_sub_spec e (Num 0) e
-| CpSub4 e1 e2 : cp_sub_spec e1 e2 (Binop Sub e1 e2).
+Definition cp_sub_spec e1 e2 e : Prop :=
+  (exists n1 n2, e1 = Num n1 /\ e2 = Num n2 /\ e = Num (n1 - n2)) \/
+  (e1 = Num 0 /\ e = Num 0) \/
+  (e1 = e /\ e2 = Num 0) \/
+  (e = Binop Sub e1 e2).
 
 Lemma cp_subP e1 e2 : cp_sub_spec e1 e2 (cp_sub e1 e2).
 Proof.
-case: e1 => [x1|[|n1]|b1 e11 e12];
-case: e2 => [x2|[|n2]|b2 e21 e22];
-eauto using cp_sub_spec.
+rewrite /cp_sub_spec.
+destruct e1 as [x1|[|n1]|b1 e11 e12]; eauto;
+destruct e2 as [x2|[|n2]|b2 e21 e22]; eauto 10.
 Qed.
 
-Inductive cp_leq_spec : expr -> expr -> expr -> Prop :=
-| CpLeq1 n1 n2 : cp_leq_spec (Num n1) (Num n2) (Num (eval_binop Leq n1 n2))
-| CpLeq2 e : cp_leq_spec (Num 0) e (Num 1)
-| CpLeq3 e1 e2 : cp_leq_spec e1 e2 (Binop Leq e1 e2).
+Definition cp_leq_spec e1 e2 e : Prop :=
+  (exists n1 n2, e1 = Num n1 /\ e2 = Num n2 /\
+                 e = Num (eval_binop Leq n1 n2)) \/
+  (e1 = Num 0 /\ e = Num 1) \/
+  (e = Binop Leq e1 e2).
 
 Lemma cp_leqP e1 e2 : cp_leq_spec e1 e2 (cp_leq e1 e2).
 Proof.
-case: e1 => [x1|[|n1]|b1 e11 e12];
-case: e2 => [x2|[|n2]|b2 e21 e22];
-eauto using cp_leq_spec.
+rewrite /cp_leq_spec.
+destruct e1 as [x1|[|n1]|b1 e11 e12]; eauto;
+destruct e2 as [x2|[|n2]|b2 e21 e22]; eauto 10.
 Qed.
 
-Inductive cp_seq_spec : com -> com -> com -> Prop :=
-| CpSeq1 c : cp_seq_spec Skip c c
-| CpSeq2 c : cp_seq_spec c Skip c
-| CpSeq3 c1 c2 : cp_seq_spec c1 c2 (Seq c1 c2).
+Definition cp_seq_spec c1 c2 c : Prop :=
+  (c1 = Skip /\ c2 = c) \/
+  (c1 = c /\ c2 = Skip) \/
+  (c = Seq c1 c2).
 
 Lemma cp_seqP c1 c2 : cp_seq_spec c1 c2 (cp_seq c1 c2).
 Proof.
-by case: c1 => *; case: c2 => *; eauto using cp_seq_spec.
+rewrite /cp_seq_spec.
+destruct c1; destruct c2; eauto 10.
 Qed.
 
-Inductive cp_assign_spec : string -> expr -> com -> Prop :=
-| CpAssign1 x : cp_assign_spec x (Var x) Skip
-| CpAssign2 x e : e ≠ Var x → cp_assign_spec x e (Assign x e).
+Definition cp_assign_spec x e c :=
+  (e = Var x /\ c = Skip) \/
+  (e <> Var x /\ c = Assign x e).
 
 Lemma cp_assignP x e : cp_assign_spec x e (cp_assign x e).
 Proof.
-case: e => [y|n|b e1 e2]; eauto using cp_assign_spec.
+rewrite /cp_assign_spec.
+destruct e as [y|n|b e1 e2]; eauto 10.
 rewrite /= bool_decide_decide.
-case: (decide (x = y)) => [->|?]; eauto using cp_assign_spec.
+destruct (decide (x = y)) as [->|?]; eauto.
 assert (Var y ≠ Var x) as ?. { congruence. }
-eauto using cp_assign_spec.
+eauto.
 Qed.
 
-Inductive cp_if_spec : expr -> com -> com -> com -> Prop :=
-| CPIf1 e c : cp_if_spec e c c c
-| CPIf2 c1 c2 : cp_if_spec (Num 0) c1 c2 c2
-| CPIf3 n c1 c2 : n ≠ 0 -> cp_if_spec (Num n) c1 c2 c1
-| CPIf4 e c1 c2 : cp_if_spec e c1 c2 (If e c1 c2).
+Definition cp_if_spec e c1 c2 c :=
+  (c1 = c2 /\ c2 = c) \/
+  (e = Num 0 /\ c2 = c) \/
+  (exists n, e = Num n /\ n <> 0 /\ c1 = c) \/
+  (c = If e c1 c2).
 
 Lemma cp_ifP e c1 c2 : cp_if_spec e c1 c2 (cp_if e c1 c2).
 Proof.
-rewrite /cp_if. case: bool_decide_reflect => [<-|_]; first exact: CPIf1.
-case: e =>[x|[|n]|*]; eauto using cp_if_spec.
+rewrite /cp_if_spec /cp_if.
+destruct (bool_decide_reflect (c1 = c2)) as [<-|_]; eauto 10.
+destruct e as [x|[|n]|*]; eauto 10.
 Qed.
 
 Definition improves {T} (x y : result T) : Prop :=
@@ -188,23 +193,29 @@ Lemma improves_bind {T S} (x y : result T) (f g : T -> result S) :
   (∀ r, improves (f r) (g r)) ->
   improves (mbind f x) (mbind g y).
 Proof.
-case=> [->|->] //= f_g. by case: y => [r||] //=.
+intros [-> | ->]; eauto; intros f_g.
+destruct y as [r| |]; eauto.
 Qed.
 
 Lemma improves_eval_expr {T} (x : result T) e s f :
   (∀ n, improves x (f n)) ->
   improves x (mbind f (eval_expr e s)).
 Proof.
-have := eval_expr_notyet e s.
-case: eval_expr => //= n.
+assert (H := eval_expr_notyet e s).
+destruct eval_expr; rewrite /=; done.
 Qed.
 
 Lemma cp_add_correct e1 e2 s :
   eval_expr (cp_add e1 e2) s = eval_expr (Binop Add e1 e2) s.
 Proof.
-case: cp_addP => [n1 n2|e|e|{}e1 {}e2] //=.
-- by case: eval_expr => // n.
-- by case: eval_expr => //= n; rewrite Nat.add_0_r.
+destruct (cp_addP e1 e2) as [H|[H|[H|H]]].
+- destruct H as (n1 & n2 & -> & -> & ->). done.
+- destruct H as (-> & <-). rewrite /=.
+  destruct eval_expr; eauto.
+- destruct H as (<- & ->). rewrite /=.
+  destruct eval_expr; eauto; rewrite /=.
+  rewrite Nat.add_0_r. done.
+- rewrite H. done.
 Qed.
 
 Lemma cp_mul_correct e1 e2 s :
@@ -213,84 +224,108 @@ Lemma cp_mul_correct e1 e2 s :
     (eval_expr (Binop Mul e1 e2) s).
 Proof.
 rewrite /=.
-case: e1 e2 _ / cp_mulP => [n1 n2|e|e|e|e|e1 e2] //=.
-- by apply: improves_eval_expr.
-- by apply: improves_eval_expr => ?; rewrite Nat.mul_0_r.
-- rewrite -{1}[eval_expr e s]result_mbind_done.
-  by apply: improves_bind => // n; rewrite Nat.add_0_r.
-- rewrite -{1}[eval_expr e s]result_mbind_done.
-  by apply: improves_bind => // n; rewrite Nat.mul_1_r.
+destruct (cp_mulP e1 e2) as [H|[H|[H|[H|[H|H]]]]].
+- destruct H as (n1 & n2 & -> & -> & ->). done.
+- destruct H as (-> & ->). rewrite /=.
+  apply improves_eval_expr. done.
+- destruct H as (-> & ->). rewrite /=.
+  apply improves_eval_expr. intros n. rewrite Nat.mul_0_r. done.
+- destruct H as (-> & <-). rewrite /=.
+  rewrite -{1}[eval_expr e2 s]result_mbind_done.
+  apply improves_bind; eauto; intros n; rewrite Nat.add_0_r.
+  done.
+- destruct H as (<- & ->).
+  rewrite -{1}[eval_expr e1 s]result_mbind_done.
+  apply improves_bind; eauto; intros n. rewrite /= Nat.mul_1_r. done.
+- rewrite H. done.
 Qed.
 
 Lemma cp_sub_correct e1 e2 s :
   improves (eval_expr (cp_sub e1 e2) s) (eval_expr (Binop Sub e1 e2) s).
 Proof.
 rewrite /=.
-case: e1 e2 _ / cp_subP => [n1 n2|e|e|e1 e2] //=.
-- by apply: improves_eval_expr => ?; rewrite Nat.sub_0_l.
-- rewrite -{1}[eval_expr e s]result_mbind_done.
+destruct (cp_subP e1 e2) as [H|[H|[H|H]]].
+- destruct H as (n1 & n2 & -> & -> & ->). done.
+- destruct H as (-> & ->). rewrite /=.
+  by apply: improves_eval_expr => ?; rewrite Nat.sub_0_l.
+- destruct H as (<- & ->). rewrite /=.
+  rewrite -{1}[eval_expr e1 s]result_mbind_done.
   by apply: improves_bind => // n; rewrite Nat.sub_0_r.
+- rewrite H. done.
 Qed.
 
 Lemma cp_leq_correct e1 e2 s :
   improves (eval_expr (cp_leq e1 e2) s) (eval_expr (Binop Leq e1 e2) s).
 Proof.
-rewrite /=; case: e1 e2 _ / cp_leqP => [n1' n2'|e|e1 e2] //=.
-by apply: improves_eval_expr.
+rewrite /=.
+destruct (cp_leqP e1 e2) as [H|[H|H]].
+- destruct H as (n1 & n2 & -> & -> & ->). done.
+- destruct H as (-> & ->). rewrite /=.
+  by apply: improves_eval_expr.
+- by rewrite H.
 Qed.
 
 Lemma cp_binop_correct s b e1 e2 :
   improves (eval_expr (cp_binop b e1 e2) s) (eval_expr (Binop b e1 e2) s).
 Proof.
-case: b.
+destruct b.
 - by rewrite cp_add_correct.
-- exact: cp_mul_correct.
-- exact: cp_sub_correct.
-- exact: cp_leq_correct.
+- apply cp_mul_correct.
+- apply cp_sub_correct.
+- apply cp_leq_correct.
 Qed.
 
 Lemma cp_expr_correct e s :
   improves (eval_expr (cp_expr e) s) (eval_expr e s).
 Proof.
-elim: e => [x|n'|b e1 IH1 e2 IH2] //=.
-apply: improves_trans.
-- exact: cp_binop_correct.
-- rewrite /=.
-  apply: improves_bind => // n1.
-  apply: improves_bind => // n2.
+induction e as [x|n'|b e1 IH1 e2 IH2]; eauto.
+eapply improves_trans. { eapply cp_binop_correct. }
+rewrite /=.
+apply improves_bind; eauto. intros n1.
+apply improves_bind; eauto.
 Qed.
 Hint Resolve cp_expr_correct : core.
 
 Lemma cp_seq_correct c1 c2 k s :
   improves (eval_com (cp_seq c1 c2) k s) (eval_com (Seq c1 c2) k s).
 Proof.
-case: c1 c2 _ / cp_seqP => [c|c|{}c1 c2] //=.
-rewrite -{1}[eval_com _ _ _]result_mbind_done.
-by apply: improves_bind => //.
+destruct (cp_seqP c1 c2) as [H|[H|H]]; rewrite /=.
+- destruct H as (-> & <-). done.
+- destruct H as (<- & ->). rewrite result_mbind_done. done.
+- rewrite H. done.
 Qed.
 
 Lemma cp_assign_correct x e k s :
   improves (eval_com (cp_assign x e) k s) (eval_com (Assign x e) k s).
 Proof.
-case: x e _ / cp_assignP => [x|x e] //=.
-case E: (s !! x) => [n|] //=. by rewrite insert_id.
+destruct (cp_assignP x e) as [H|H].
+- destruct H as (-> & ->). rewrite /=.
+  destruct (s !! x) as [n|] eqn:E; rewrite /=; eauto.
+  rewrite insert_id; done.
+- destruct H as (_ & ->). done.
 Qed.
 
 Lemma cp_if_correct e c1 c2 k s :
   improves (eval_com (cp_if e c1 c2) k s) (eval_com (If e c1 c2) k s).
 Proof.
-rewrite /=.
-case: e c1 c2 _ / cp_ifP=> [e c|c1 c2|n c1 c2 n_not_0|e c1 c2] //=.
-- apply: improves_eval_expr => //= n. by case: bool_decide.
-- by rewrite bool_decide_eq_false_2.
+destruct (cp_ifP e c1 c2) as [H|[H|[H|H]]]; rewrite /=.
+- destruct H as (<- & <-).
+  apply improves_eval_expr. intros n.
+  destruct bool_decide; done.
+- destruct H as (-> & <-). done.
+- destruct H as (n & -> & n0 & <-). rewrite /=.
+  rewrite bool_decide_eq_false_2; done.
+- rewrite H. done.
 Qed.
 
 Lemma cp_while_correct e c k s :
   improves (eval_com (cp_while e c) k s) (eval_com (While e c) k s).
 Proof.
 rewrite /cp_while.
-case: bool_decide_reflect => [->|] //.
-by case: k.
+destruct bool_decide eqn:E.
+- rewrite bool_decide_eq_true in E. rewrite E /=.
+  destruct k; done.
+- done.
 Qed.
 
 Lemma iter_improves {T} (f g : (T -> result T) -> T -> result T) x k :
@@ -298,22 +333,24 @@ Lemma iter_improves {T} (f g : (T -> result T) -> T -> result T) x k :
              ∀ x, improves (f f' x) (g g' x)) ->
   improves (iter f k x) (iter g k x).
 Proof.
-move=> H; elim: k => //= [|k IH] in x *; apply: H => // {}x.
+intros H.
+induction k as [|k IH] in x |- *; rewrite /=; apply H; eauto.
 Qed.
 
 Lemma cp_com_correct c k s :
   improves (eval_com (cp_com c) k s) (eval_com c k s).
 Proof.
-elim: c => [|c1 IH1 c2 IH2|x e|e c1 IH1 c2 IH2|e c IH] //= in s k *.
-- apply: improves_trans; first exact: cp_seq_correct.
-  by rewrite /=; apply: improves_bind => //.
-- apply: improves_trans; first exact: cp_assign_correct.
-  by rewrite /=; apply: improves_bind => //.
-- apply: improves_trans; first exact: cp_if_correct.
-  rewrite /=; apply: improves_bind => // n.
-  by case: bool_decide.
-- apply: improves_trans; first exact: cp_while_correct.
-  rewrite /=. apply: iter_improves=> f g f_g {}s.
-  apply: improves_bind => // - [|n] //=.
-  apply: improves_bind => //=.
+induction c as [|c1 IH1 c2 IH2|x e|e c1 IH1 c2 IH2|e c IH] in s, k |- *.
+- done.
+- rewrite /=. eapply improves_trans. { eapply cp_seq_correct. }
+  rewrite /=. apply improves_bind; eauto.
+- rewrite /=. eapply improves_trans. { eapply cp_assign_correct. }
+  rewrite /=. apply improves_bind; eauto.
+- rewrite /=. eapply improves_trans. { eapply cp_if_correct. }
+  rewrite /=. apply improves_bind; eauto. intros n.
+  destruct bool_decide; eauto.
+- rewrite /=. eapply improves_trans. { eapply cp_while_correct. }
+  rewrite /=. apply iter_improves. intros f g f_g s'.
+  apply improves_bind; eauto. intros [|n]; rewrite /=; eauto.
+  apply improves_bind; eauto.
 Qed.
